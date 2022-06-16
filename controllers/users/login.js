db78192dac640890d36305f6212e74e5bb2d969c
rishi1717 <rshrrrss@gmail.com>
2022-06-16 10:57:51 +0530

    jwt send

--- controllers/users/login.js
+++ controllers/users/login.js
@@ -8,31 +8,30 @@ const login = async (req, res) => {
 		if (error) {
 			return res.status(400).send(error.details[0].message)
 		}
-		const user = await Users.findOne({
+		let user = await Users.findOne({
 			userName: req.body.userName,
-		})
-        if (!user) {
-            return res.status(401).send("Invalid userName or password")
-        }
-        const password = await bcrypt.compare(req.body.password, user.password)
-        if (!password) {
-            return res.status(401).send("Invalid userName or password")
-        }
-        const token = user.generateToken(user)
-        res.status(200).cookie("jwt_token", token,{
-            httpOnly: true,
-            secure: process.env.NODE_ENV === "production",
-        }).send("Login successful")
+		},{ createdAt:0, updatedAt:0, __v:0 })
+		if (!user) {
+			return res.status(401).send("Invalid userName or password")
+		}
+		const password = await bcrypt.compare(req.body.password, user.password)
+		if (!password) {
+			return res.status(401).send("Invalid userName or password")
+		}
+		const token = user.generateToken(user)
+        user.password = undefined
+        user = {...user._doc, token}
+		res.status(200).send({ user, message: "Login successful" })
 	} catch (err) {
 		res.status(500).send(err.message)
 	}
 }
 
 const validate = (data) => {
-    const schema = joi.object({
-        userName: joi.string().required().label('userName'),    
-        password: joi.string().required().label('password'),
-    })
-    return schema.validate(data)
+	const schema = joi.object({
+		userName: joi.string().required().label("userName"),
+		password: joi.string().required().label("password"),
+	})
+	return schema.validate(data)
 }
 export default login
