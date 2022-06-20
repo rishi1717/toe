2740aaff7675aaea46ded83f7aedb1064104da8c
rishi1717 <rshrrrss@gmail.com>
2022-06-20 15:30:57 +0530

    windows

--- controllers/users/authUser.js
+++ controllers/users/authUser.js
@@ -7,21 +7,42 @@ const client = new OAuth2Client({
 })
 
 const authUser = async (req, res) => {
-	const { token } = req.body
-	const ticket = await client.verifyIdToken({
-		idToken: token,
-		audience: process.env.GOOGLE_CLIENT_ID,
-	})
-	const payload = ticket.getPayload()
-	let user = await Users.findOne({ email: payload?.email })
-	if (!user) {
-		user = await new Users({
-			email: payload?.email,
-			name: payload?.name,
+	try {
+		const { token } = req.body
+		const ticket = await client.verifyIdToken({
+			idToken: token,
+			audience: process.env.GOOGLE_CLIENT_ID,
 		})
-		await user.save()
+		const payload = ticket.getPayload()
+		let userName = payload.given_name
+
+		let user = await Users.findOne({ email: payload?.email })
+		if (!user) {
+			async function getUserName(userName) {
+				const userExists = await Users.exists({ userName: userName })
+				if (userExists) {
+					function getRandomInt(max) {
+						return Math.floor(Math.random() * Math.floor(max))
+					}
+					userName = userName + getRandomInt(9999)
+					getUserName(userName)
+				}
+				return userName
+			}
+			userName = await getUserName(userName)
+			console.log(userName)
+			user = await new Users({
+				email: payload.email,
+				fullName: payload.name,
+				userName: userName,
+			})
+			await user.save()
+			res.send({message:"user created", user})
+		}
+		res.send({ user, token, message: "user authorized" })
+	} catch (err) {
+		console.log(err.message)
 	}
-	res.json({ user, token })
 }
 
 export default authUser
