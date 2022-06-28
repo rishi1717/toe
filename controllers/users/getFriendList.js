4dc30518dbf540b139f4ef8154c79143e74f6d09
rishi1717 <rshrrrss@gmail.com>
2022-06-28 15:38:05 +0530

    friends list

---
+++ controllers/users/getFriendList.js
@@ -0,0 +1,15 @@
+import Users from "../../models/userModel.js"
+
+const getFriendList = async (req, res) => {
+	try {
+		const user = await Users.findById(req.params.id)
+        const friendlist = user.friends
+		const friends = await Users.find({ _id: { $in: user.friends } })
+		res.status(200).json(friends)
+	} catch (err) {
+		console.log(err.message)
+		res.status(500).send(err.message)
+	}
+}
+
+export default getFriendList
--- routes/userRoutes.js
+++ routes/userRoutes.js
@@ -2,6 +2,7 @@ import {Router} from 'express'
 import register from '../controllers/users/register.js'
 import login from '../controllers/users/login.js'
 import userSearch from '../controllers/users/userSearch.js'
+import getFriendList from '../controllers/users/getFriendList.js'
 
 const router = Router()
 
@@ -10,5 +11,7 @@ router.post('/', register)
 router.post("/login", login)
 
 router.get('/', userSearch)
+
+router.get('/friends/:id',getFriendList)
 
 export default router
