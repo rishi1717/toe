4cda26e082a2a33219a01dc956e67bea6c80badf
rishi1717 <rshrrrss@gmail.com>
2022-07-03 18:31:45 +0530

    friend req list

---
+++ controllers/friendReq/getRequests.js
@@ -0,0 +1,13 @@
+import FriendRequests from "../../models/friendRequests.js"
+
+const getRequests = async (req, res) => {
+	try {
+		const requests = await FriendRequests.find({ to: req.params.id })
+        res.status(200).json(requests)
+	} catch (err) {
+		console.log(err.message)
+		res.status(500).json({ message: "Something went wrong!" })
+	}
+}
+
+export default getRequests
--- routes/friendReqRoutes.js
+++ routes/friendReqRoutes.js
@@ -2,12 +2,15 @@ import { Router } from "express"
 import acceptReq from "../controllers/friendReq/acceptReq.js"
 import cancelRequest from "../controllers/friendReq/deleteReq.js"
 import friendRequest from "../controllers/friendReq/friendRequest.js"
+import getRequests from "../controllers/friendReq/getRequests.js"
 import rejectReq from "../controllers/friendReq/rejectReq.js"
 import removeFriend from "../controllers/friendReq/removeFriend.js"
 
 const router = Router()
 
 router.post("/", friendRequest)
+
+router.get("/:id", getRequests)
 
 router.patch("/accept/:id", acceptReq)
 
