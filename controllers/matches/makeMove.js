541fc90a7f592ceaffb4e3f52486d413246e2bee
rishi1717 <rshrrrss@gmail.com>
2022-07-13 23:27:13 +0530

    game won bug remaining

--- controllers/matches/makeMove.js
+++ controllers/matches/makeMove.js
@@ -14,7 +14,9 @@ const lines = [
 const makeMove = async (req, res) => {
 	try {
 		let winner1 = false,
-			winner2 = false
+			winner2 = false,
+			points1 = false,
+			points2 = false
 		const { player, move } = req.body
 		const matchId = req.params.id
 		let match = await Matches.findByIdAndUpdate(
@@ -25,38 +27,7 @@ const makeMove = async (req, res) => {
 				},
 			},
 			{ new: true }
-		).populate("player1 player2")
-
-		for (let line of lines) {
-			winner1 = line.every((index) => {
-				return match.player1Moves.includes(index)
-			})
-			winner2 = line.every((index) => {
-				return match.player2Moves.includes(index)
-			})
-			if (winner1) {
-				match = await Matches.findByIdAndUpdate(
-					matchId,
-					{
-						$set: { winner: match.player1 },
-					},
-					{ new: true }
-				).populate("player1 player2 winner")
-				global.io.emit("matchUpdate", match)
-				return res.status(200).json({ message: "Match won!", match })
-			}
-			if (winner2) {
-				match = await Matches.findByIdAndUpdate(
-					matchId,
-					{
-						$set: { winner: match.player2 },
-					},
-					{ new: true }
-				).populate("player1 player2 winner")
-				global.io.emit("matchUpdate",{})
-				return res.status(200).json({ message: "Match won!", match })
-			}
-		}
+		).populate("player1 player2 winner")
 
 		if (!match) {
 			return res.status(404).json({ message: "Match not found" })
@@ -64,6 +35,65 @@ const makeMove = async (req, res) => {
 		if (match.status !== "accepted") {
 			return res.status(400).json({ message: "Match not accepted" })
 		}
+
+		winner1 = match.player1Points === match.pointsToWin
+		winner2 = match.player2Points === match.pointsToWin
+		if (winner1) {
+			match = await Matches.findByIdAndUpdate(
+				matchId,
+				{
+					$set: { winner: match.player1 },
+				},
+				{ new: true }
+			).populate("player1 player2 winner")
+			global.io.emit("matchUpdate")
+			return res.status(200).json({ message: "Match won!", match })
+		}
+		if (winner2) {
+			match = await Matches.findByIdAndUpdate(
+				matchId,
+				{
+					$set: { winner: match.player2 },
+				},
+				{ new: true }
+			).populate("player1 player2 winner")
+			global.io.emit("matchUpdate")
+			return res.status(200).json({ message: "Match won!", match })
+		}
+
+		for (let line of lines) {
+			points1 = line.every((index) => {
+				return match.player1Moves.includes(index)
+			})
+			points2 = line.every((index) => {
+				return match.player2Moves.includes(index)
+			})
+			if (points1) {
+				match = await Matches.findByIdAndUpdate(
+					{ _id: matchId },
+					{
+						$inc: { player1Points: 1 },
+						$set: { player1Moves: [], player2Moves: [] },
+					},
+					{ new: true }
+				).populate("player1 player2 winner")
+				global.io.emit("pointUpdate")
+				return res.status(200).json({ message: "Points won!", match })
+			}
+			if (points2) {
+				match = await Matches.findByIdAndUpdate(
+					{ _id: matchId },
+					{
+						$inc: { player2Points: 1 },
+						$set: { player1Moves: [], player2Moves: [] },
+					},
+					{ new: true }
+				).populate("player1 player2 winner")
+				global.io.emit("pointUpdate")
+				return res.status(200).json({ message: "Points won!", match })
+			}
+		}
+		global.io.emit("matchUpdate")
 		res.status(200).send({ message: "Move made!", match })
 	} catch (err) {
 		console.log(err.message)
--- controllers/matches/matchRequest.js
+++ controllers/matches/matchRequest.js
@@ -2,18 +2,19 @@ import Matches from "../../models/matchModel.js"
 
 const matchRequest = async (req, res) => {
 	try {
-		const { player1, player2, entryFee } = req.body
-		const winningAmount = entryFee * 2 * (9 / 10)
-        const income = entryFee * 2 * (1 / 10)
-		await new Matches({
+		const { player1, player2, entryFee, pointsToWin, winningAmount } = req.body
+		const income = entryFee*2 - winningAmount
+		const { _id } = await new Matches({
 			player1,
 			player2,
 			entryFee,
+			pointsToWin,
 			winningAmount,
 			income,
 			status: "requested",
 		}).save()
-		const match = await Matches.findOne({ player1, player2 }).populate("player1 player2")
+
+		const match = await Matches.findOne({ _id }).populate("player1 player2")
 		res.status(200).json({ message: "Match request sent!", match })
 	} catch (err) {
 		console.log(err.message)
